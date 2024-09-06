679116e29b08efb66f299a731001c73792162d2e
ingyin-san <ingyinsan7700@gmail.com>
2024-09-06 21:25:52 +0630

    Create Point.java

---
+++ CodingExercise33/Point.java
@@ -0,0 +1,40 @@
+public class Point {
+    private int x;
+    private int y;
+    
+    public Point() {
+    }
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public double distance() {
+        return Math.sqrt((0-this.x)*(0-this.x)+(0-this.y)*(0-this.y));
+    }
+    
+    public double distance(Point p) {
+        return Math.sqrt((p.x-this.x)*(p.x-this.x)+(p.y-this.y)*(p.y-this.y));
+    }
+    
+    public double distance(int x, int y) {
+        return Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
+    }
+}
