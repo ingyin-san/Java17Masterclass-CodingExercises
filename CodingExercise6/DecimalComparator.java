447cc14e2b048a27f5d119ba12b3dd0da76fac4f
ingyin-san <ingyinsan7700@gmail.com>
2024-09-06 20:37:15 +0630

    Create DecimalComparator.java

---
+++ CodingExercise6/DecimalComparator.java
@@ -0,0 +1,5 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces (double first, double second) {
+        return (int) (first * 1000) == (int) (second * 1000);
+    }
+}
