e3c96a346d5e168b26cc77985a606063aacb2b33
ingyin-san <ingyinsan7700@gmail.com>
2024-09-06 20:44:38 +0630

    Create PlayingCat.java

---
+++ CodingExercise12/PlayingCat.java
@@ -0,0 +1,5 @@
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        return temperature >= 25 && temperature <= (summer ? 45 : 35);
+    }
+}
